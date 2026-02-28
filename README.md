8f02d9beda6e613e179634145051d03cbe62b154
46009361 <165697051+46009361@users.noreply.github.com>
2026-02-28 04:49:56 +0000

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GPT-5 Playground
+
+Bypass GPT-5 resource limits imposed on chatgpt.com!
+
+This is a model that "thinks" for a while, so please expect slower responses.
---
+++ script.js
@@ -0,0 +1,38 @@
+const messages = document.forms.messages;
+messages.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const arr = [], obj = {};
+    Array.from(messages).forEach(element => {
+        if (!element.disabled && element.name) {
+            obj.role = element.name;
+            obj.content = element.value;
+        }
+        arr.push(obj);
+    });
+    // let's deplete my school's resources!
+    const response = await fetch("https://corsproxy.io/?url=https://wcln-ai.twostoryapps.com/letschat&key=🍪🍪🍪🍪",
+        {
+            headers: {
+                "content-type": "application/json",
+                host: "wcln-ai.twostoryapps.com"
+            },
+            method: "POST",
+            body: JSON.stringify({messages: arr})
+        }
+    );
+    try {
+        const json = await response.json();
+        if (json?.success) {
+            messages.assistant.value = json?.data?.choices[0]?.message?.content;
+            messages.assistant.placeholder = "If you see this, the AI returned a blank response";
+        } else {
+            throw new SyntaxError("success is missing or not true");
+        }
+    }
+    catch (error) {
+        alert(error);
+    }
+    finally {
+        return true;
+    }
+});
